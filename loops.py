1a84c579c8e4a164545d455fe6dbeab0cb122011
Raghav bhola <raghavbhola211@gmail.com>
2025-10-23 22:19:15 +0530

    Add files via upload

---
+++ loops.py
@@ -0,0 +1,6 @@
+#L00PS:->(for,while,for else,nested loops,break,continue,pass)
+#(1)For loop->string,list,tuple,set,dictionary
+s=[15,26,32,44,58,43,78,90,11,23]
+for i in range(len(s)):
+    print(i,s[i],sep="->")
+
