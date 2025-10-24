8ed38cc6fddeaa83aabcb3f0aca145e224c05fd3
Raghav bhola <raghavbhola211@gmail.com>
2025-10-24 21:58:08 +0530

    Add files via upload

--- loops.py
+++ loops.py
@@ -4,3 +4,27 @@ s=[15,26,32,44,58,43,78,90,11,23]
 for i in range(len(s)):
     print(i,s[i],sep="->")
 
+s="This is thirsty crow,looking for a drop of water and it is very hot day"
+#s1=10
+#for i in s1:    
+    #print(i)
+for i in s:
+    print(i,end="")
+
+s1=str(1001)
+print(type[s1])    
+print(s1[3])       #set
+for i in s1:
+    print(i,end=".")
+
+s= "Beautiful is better than ugly, Explicit is better than implicit,Simple is better than completed,Flat is beeter than nested,sparse is better than dence,Readability counts"
+for i in s:
+    if i not in "aeiouAEIOU":  #if it returns true,it will print consonats
+        print(i,end="")
+
+sum_tuple=(5,26,32,44,58,43,78,90,11,23)   #tuple
+total=0
+for i in sum_tuple:                    #loop for tuple
+    print(i,total,sep="->")
+    #total=total+i
+    #print(i,total,sep="->")
