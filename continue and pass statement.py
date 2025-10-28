2ed1702522866831ee5b8d707d96bc822aecc11e
Raghav bhola <raghavbhola211@gmail.com>
2025-10-28 22:22:10 +0530

    Add files via upload

--- continue and pass statement.py
+++ continue and pass statement.py
@@ -6,3 +6,25 @@ for letter in "PythonisaAmazinglanguagewhichisveryeasytolearnusegreatlyandalsove
         print("found on 'A' or 'a',skipping..")
         continue      #skips the current iteration
     print(letter,end="-")  #print all letters in a single line with space.
+#output 
+'''
+P-y-t-h-o-n-i-s-found on 'A' or 'a',skipping..
+found on 'A' or 'a',skipping..
+m-found on 'A' or 'a',skipping..
+z-i-n-g-l-found on 'A' or 'a',skipping..
+n-g-u-found on 'A' or 'a',skipping..
+g-e-w-h-i-c-h-i-s-v-e-r-y-e-found on 'A' or 'a',skipping..
+s-y-t-o-l-e-found on 'A' or 'a',skipping..
+r-n-u-s-e-g-r-e-found on 'A' or 'a',skipping..
+t-l-y-found on 'A' or 'a',skipping..
+n-d-found on 'A' or 'a',skipping..
+l-s-o-v-e-r-y-p-o-w-e-r-f-u-l-   '''
+
+#Pass Statement:->It is used to avoid getting errors when a statement is required syntactically but you don't want to do anything.
+#(1)
+for letter in "PythonIsAmazingLanguage":
+    if letter=="g":
+        pass      #does nothing,avoids error.
+    print(letter,end="-")
+#output
+#P-y-t-h-o-n-I-s-A-m-a-z-i-n-g-L-a-n-g-u-a-g-e-
