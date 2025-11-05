a11da212b562fa4e357c68a78d663cc8e78deb95
Raghav bhola <raghavbhola211@gmail.com>
2025-11-05 18:03:55 +0530

    Add files via upload

---
+++ Python Module's.py
@@ -0,0 +1,9 @@
+#Python Modules:-A file containing python code, Eg.-Function and variable, that can be ignored and used in other python program.
+import math
+print(math.sqrt(16))
+print(math.pi)
+print(math.sqrt(25))
+
+#import mymodule
+
+#mymodule.print_message("Alice")
