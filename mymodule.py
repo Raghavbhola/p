d66ece2b522768303562b9052c8fed0539771378
Raghav bhola <raghavbhola211@gmail.com>
2025-11-06 19:38:05 +0530

    Add files via upload

---
+++ mymodule.py
@@ -0,0 +1,30 @@
+def print_message(name):
+    return"Hello,"+name+"welcome to my module"
+ 
+def Sum_numbers(a,b):
+    return a+b
+
+def divide_numbers(a,b):
+    return a/b
+ 
+def divide_numbers(a,b):
+    if b!=0:
+        return a/b
+    else:
+        return "error:division by zero is not allowed"
+
+def multiply_numbers(a,b):
+    return a*b 
+
+def subtract_numbers(a,b):
+    return a-b 
+
+def power_numbers(a,b):
+    return a**b
+
+def modulus_numbers(a,b):
+    return a%b
+
+def floor_divide_numbers(a,b):
+    return a//b
+    
