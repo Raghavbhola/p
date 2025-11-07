ca1c5ca9b0a9a4aa4a5145b3005a48a3ba07c065
Raghav bhola <raghavbhola211@gmail.com>
2025-11-07 12:17:55 +0530

    Add files via upload

--- Python Module's.py
+++ Python Module's.py
@@ -11,11 +11,38 @@ print(mymodule.print_message(" Alice "))       #Hello, Alice welcome to my modul
 print(mymodule.print_message(" Bob "))         #Hello, Bob welcome to my module
 print(mymodule.print_message(" Microsoft "))   #Hello, Microsoft welcome to my module
 print("Hello Buddy,How are you!") '''             #Hello Buddy,How are you!
-
-from importlib import reload
+#(3)------------------------------------------->
+'''from importlib import reload
 import mymodule
 reload(mymodule)
 
 print(mymodule.print_message(" Alice "))                       #Hello, Alice welcome to my module
 print(mymodule.print_message(" MY Baby!how are you  & "))      #Hello, Hello! MY Baby!how are you  &  welcome to my module
-print(mymodule.print_message(" Hello! Karan Aujla "))          #Hello, Hello! Karan Aujla welcome to my module
+print(mymodule.print_message(" Hello! Karan Aujla ")) '''         #Hello, Hello! Karan Aujla welcome to my module
+
+'''from importlib import reload
+import mymodule
+
+print(mymodule.print_message(" Karan Aujla "))
+print(mymodule.Sum_numbers(5,10))             #15
+print(mymodule.multiply_numbers(5,10))        #50
+print(mymodule.subtract_numbers(10,2))        #8
+print(mymodule.power_numbers(5,2))            #25
+print(mymodule.divide_numbers(10,2))          #5.0
+print(mymodule.multiply_numbers(7000000,90000))     #630000000000
+print(mymodule.power_numbers(9000,10))   '''           #3486784401000000000000000000000000000000
+#--------------------------------------------------------------------------------------------------------------
+#**NOTE-[*] use krne se Sab kuch select krdega.
+#ALIAS- You are referring to the same thing.
+#*Directory means:-[Folder] **dir:-If particular module, Which functions exist in it?
+#(4)--------------------------------------------------------------------------------->
+import mymodule as mm   #import with alias
+
+print(mm.print_message(" Karan Aujla "))     #Hello, Karan Aujla welcome to my module
+print(mm.Sum_numbers(99,5))                  #104
+print(mm.multiply_numbers(88,67))            #5896
+print(mm.subtract_numbers(87,98))            #-11
+print(mm.power_numbers(8,7))                 #2097152
+print(mm.divide_numbers(90,7))               #12.857142857142858
+
+print(dir(mm))
