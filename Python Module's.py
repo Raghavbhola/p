0d46135957cc70a254039662ad1ce600707f1aff
Raghav bhola <raghavbhola211@gmail.com>
2025-11-09 17:16:36 +0530

    Add files via upload

--- Python Module's.py
+++ Python Module's.py
@@ -5,22 +5,22 @@
 #print(math.pi)
 #print(math.sqrt(25))
 #(2)----------------------------------**Print krvane ke liye alag se new file bananni hoegi:-mymodule.py--------------------- 
-'''import mymodule
+import mymodule
  
 print(mymodule.print_message(" Alice "))       #Hello, Alice welcome to my module
 print(mymodule.print_message(" Bob "))         #Hello, Bob welcome to my module
 print(mymodule.print_message(" Microsoft "))   #Hello, Microsoft welcome to my module
-print("Hello Buddy,How are you!") '''             #Hello Buddy,How are you!
+print("Hello Buddy,How are you!")              #Hello Buddy,How are you!
 #(3)------------------------------------------->
-'''from importlib import reload
+from importlib import reload
 import mymodule
 reload(mymodule)
 
 print(mymodule.print_message(" Alice "))                       #Hello, Alice welcome to my module
 print(mymodule.print_message(" MY Baby!how are you  & "))      #Hello, Hello! MY Baby!how are you  &  welcome to my module
-print(mymodule.print_message(" Hello! Karan Aujla ")) '''         #Hello, Hello! Karan Aujla welcome to my module
+print(mymodule.print_message(" Hello! Karan Aujla "))          #Hello, Hello! Karan Aujla welcome to my module
 
-'''from importlib import reload
+from importlib import reload
 import mymodule
 
 print(mymodule.print_message(" Karan Aujla "))
@@ -30,13 +30,13 @@ print(mymodule.subtract_numbers(10,2))        #8
 print(mymodule.power_numbers(5,2))            #25
 print(mymodule.divide_numbers(10,2))          #5.0
 print(mymodule.multiply_numbers(7000000,90000))     #630000000000
-print(mymodule.power_numbers(9000,10))   '''           #3486784401000000000000000000000000000000
+print(mymodule.power_numbers(9000,10))              #3486784401000000000000000000000000000000
 #--------------------------------------------------------------------------------------------------------------
 #**NOTE-[*] use krne se Sab kuch select krdega.
 #ALIAS- You are referring to the same thing.
 #*Directory means:-[Folder] **dir:-If particular module, Which functions exist in it?
 #(4)--------------------------------------------------------------------------------->
-import mymodule as mm   #import with alias
+import mymodule as mm   #import with alias  *Nick name of mymodule:->mm
 
 print(mm.print_message(" Karan Aujla "))     #Hello, Karan Aujla welcome to my module
 print(mm.Sum_numbers(99,5))                  #104
@@ -45,4 +45,10 @@ print(mm.subtract_numbers(87,98))            #-11
 print(mm.power_numbers(8,7))                 #2097152
 print(mm.divide_numbers(90,7))               #12.857142857142858
 
-print(dir(mm))
+print(dir(mm))           #**It only prints the alphabet.
+#(5)------------>
+import math 
+print(dir(math))
+
+import matplotlib
+print(dir(matplotlib))
