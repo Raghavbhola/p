4cde5b890b94fa5e4efe0a25c79c91eb9042981d
Raghav bhola <raghavbhola211@gmail.com>
2025-12-02 12:19:10 +0530

    Add files via upload

---
+++ Practice.py
@@ -0,0 +1,17 @@
+#Min of 2 numbers:-
+a=int(input("enter a number"))
+b=int(input("enter a number"))
+
+if a<b:
+    print(a)
+else:
+    print(b)
+
+
+#is number odd or even:
+n=int(input("enter a number"))
+
+if n%2 == 0:
+    print(f"This is even numbber:-")
+else:
+    print(f"This is an odd number:-0")    
