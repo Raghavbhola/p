605b6f0bc9032ab07301048f026b6e7826a6355e
Raghav bhola <raghavbhola211@gmail.com>
2025-12-02 12:31:37 +0530

    Add files via upload

--- Practice.py
+++ Practice.py
@@ -7,7 +7,7 @@ if a<b:
 else:
     print(b)
 
-
+#--------------------------------->
 #is number odd or even:
 n=int(input("enter a number"))
 
@@ -15,3 +15,13 @@ if n%2 == 0:
     print(f"This is even numbber:-")
 else:
     print(f"This is an odd number:-0")    
+
+#sum of numbers from 1 to N:->
+N=int(input("enter a number"))
+count=1
+sum=0
+while count<=N:
+    sum=sum+count
+    count=count+1
+
+print(sum)
