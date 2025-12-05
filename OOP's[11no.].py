3547b0f98d1b25d68482c53197631dd9c2fbfbb9
Raghav bhola <raghavbhola211@gmail.com>
2025-12-05 17:35:14 +0530

    Add files via upload

---
+++ OOP's[11no.].py
@@ -0,0 +1,7 @@
+# OOP's[Object Oriented Program]
+# Defn:->To map with real world scenarios, we started using objects in code. This is called OOP's
+
+#NOTE- OOP is a approach not a coding.
+#      OOP's are aapproach of programming or We tie him up as an object.
+#      OOP's a POL i.e.[Paradigm] it is approach/concept.
+#POL:->Procedural Oriented program Paradigm (program describes procedure of performing certain task ny written a series of instruction in a logical order)
