c59676f10b73089e47fca720c376bf06d3d5affd
Raghav bhola <raghavbhola211@gmail.com>
2025-12-05 17:57:26 +0530

    Add files via upload

--- OOP's[11no.].py
+++ OOP's[11no.].py
@@ -5,3 +5,18 @@
 #      OOP's are aapproach of programming or We tie him up as an object.
 #      OOP's a POL i.e.[Paradigm] it is approach/concept.
 #POL:->Procedural Oriented program Paradigm (program describes procedure of performing certain task ny written a series of instruction in a logical order)
+
+#*Class & Object:-> *class is a blue print for creating objects.
+#                 Ex:->   #creating class
+#                      Class student:
+#                         name="Karan Kumar"
+#*Object refers to a real-world entity with state and behavior.
+# Attribute+Behavior:->single-unit millke banati hai class
+
+#Principle of OOP's:->
+#(1)class
+#(2)object
+#(3)Encapsulation
+#(4)Inheritance
+#(5)Polymorphism
+#(6)Abstruction
