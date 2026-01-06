32ef751dc062411085e9cecc6be95d6cb40d42c5
Raghav bhola <raghavbhola211@gmail.com>
2026-01-06 16:45:39 +0530

    Add files via upload

--- Practice.py
+++ Practice.py
@@ -16,6 +16,7 @@ if n%2 == 0:
 else:
     print(f"This is an odd number:-0")    
 
+#--------------------------
 #sum of numbers from 1 to N:--->
 N=int(input("enter a number"))
 count=1
@@ -25,3 +26,15 @@ while count<=N:
     count=count+1
 
 print(sum)
+
+#---------------------------------------------
+#to check it is prime number or not,if it is prime, check its a odd numberor a even number?
+num =int(input("enter a number to check prime or not"))
+for i in range (2,num):
+   if (num%i)==0:
+      print(f"{num}%{i}={num%i}")
+      print(num,"is not a prime number")
+      break
+   else:
+      print(num,"is a prime number")
+   
