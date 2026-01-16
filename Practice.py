12aeb4bcd4d7d93a16ff04ebab51892f369163c8
Raghav bhola <raghavbhola211@gmail.com>
2026-01-16 21:49:48 +0530

    Add files via upload

--- Practice.py
+++ Practice.py
@@ -83,5 +83,6 @@ def factorial_recursive(n):
 print("factorial of", num ,"is:-", factorial_recursive(num))
     
 #---------------
-# Write a Python program to reverse a string without using built-in functions like [::-1]?
+# Write a Python program to reverse a string without using built-in functions like? at least 4-5 character.
+
     
