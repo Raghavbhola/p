3c58fd62f44059c8d2da3ac21476f887521c3cec
Raghav bhola <raghavbhola211@gmail.com>
2026-01-29 23:44:24 +0530

    Add files via upload

--- Practice.py
+++ Practice.py
@@ -87,7 +87,7 @@ print("factorial of", num ,"is:-", factorial_recursive(num))
 y = "python is high level language"
 print(y[::-1])   
 
-x = "hello world! Python"
+x = "hello world! Pop language"
 print(x[::-1])
 
 z ="this is my excersice notebook"
