93747d89e480fda31a95892950aac78b1a350c9e
Raghav bhola <raghavbhola211@gmail.com>
2026-01-31 15:05:02 +0530

    Add files via upload

--- Practice.py
+++ Practice.py
@@ -101,10 +101,8 @@ print(f[::-1])
 
 #Without using any string methods, try to print the following: 123....n
 n = int(input())
-    
 for i in range(1,n+1):
-        
-    print(i,end="")
+        print(i,end="")
 
 #Swapcase with using decorator----------------------------->
 def swap_case(arg):
